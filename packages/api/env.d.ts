ba4cef06ae14d580f9788b37ea88f6e3153833b1
Traian Stanciu <traian.stanciu@arnia.ro>
2024-05-23 17:10:24 +0300

    chore/db_seeder_and_env_updates

--- packages/api/env.d.ts
+++ packages/api/env.d.ts
@@ -1,7 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
+    AWS_REGION: string;
+    AWS_ACCESS_KEY_ID: string;
+    AWS_SECRET_ACCESS_KEY: string;
     APP_MEMORY_CLASS: string;
+    DYNAMODB_ENDPOINT: string;
+    DYNAMODB_TABLE_MESSAGES: string;
     MYSQL_HOST: string;
     MYSQL_USER: string;
     MYSQL_PASSWORD: string;
